3b9b69681bcd3d53ad55beb92c61d2780352c8a3
Zih Ci Lai <zihcilai@gmail.com>
2017-05-29 17:31:15 +0800

    Initial Commit

---
+++ MyOneSignal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOneSignal
+//
+//  Created by Lai Zih Ci on 2017/5/29.
+//  Copyright © 2017年 ZihCi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
